5125aba10cf14f699a173966625898d5828845ef
Furkad <71381867+Furkad@users.noreply.github.com>
2020-09-21 12:29:31 +0300

    Add files via upload

---
+++ vvp/vvp4/vvp4.4.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	float a = 14, b = -1.8;
+	double sum, min, del, umn;
+	sum = a * a + b * b;
+	min = a * a - b * b;
+	del = (a * a) / (b * b);
+	umn = (a * a) * (b * b);
+	cout << sum << endl;
+	cout << min << endl;
+	cout << del << endl;
+	cout << umn << endl;
+	return 0;
+}
