c4a40ed4d086cce2c79dff3b6755dba87fe61f7d
Furkad <71381867+Furkad@users.noreply.github.com>
2020-09-23 13:17:22 +0300

    Add files via upload

---
+++ vvp/vvp5/vvp.5.2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <locale.h>
+#include <math.h>
+
+int main() {
+	float a, b, c;
+	double ac, bc, sum;
+	setlocale(LC_ALL, "Russian");
+	std::cout << "Введите координаты точек A, B, C\n";
+	std::cin >> a >> b >> c;
+	ac = abs(c - a);
+	bc = abs(b - a);
+	sum = ac + bc;
+	std::cout << ac << " " << bc << " "  << sum;
+	return 0;
+}
