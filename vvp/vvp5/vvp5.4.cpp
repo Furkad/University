e5ff770d15ea5431d80a37f8517a742a2340cbbf
Furkad <71381867+Furkad@users.noreply.github.com>
2020-09-23 16:08:50 +0300

    Add files via upload

---
+++ vvp/vvp5/vvp5.4.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <locale.h>
+#include <cmath>
+
+int main() {
+	float x1, x2, y1, y2, a, b;
+	double p, s;
+	setlocale(LC_ALL, "Russian");
+	std::cout << "Введите координаты первой точки: ";
+	std::cin >> x1 >> y1;
+	std::cout << "Введите координаты второй точки: ";
+	std::cin >> x2 >> y2;
+	a = abs(x2 - x1);
+	b = abs(y2 - y1);
+	p = 2 * (a + b);
+	s = a * b;
+	std::cout << "P = " << p << " S = " << s;
+	return 0;
+}
