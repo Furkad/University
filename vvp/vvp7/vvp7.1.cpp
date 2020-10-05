4777744e0fdb3dc2ff86e85824941f780b33e103
Furkad <71381867+Furkad@users.noreply.github.com>
2020-10-05 12:17:26 +0300

    Add 7.1 and 7.2

---
+++ vvp/vvp7/vvp7.1.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <locale.h>
+
+int main(){
+    const double pi = 3.14;
+    float x;
+    double res;
+    setlocale(LC_ALL, "Russian");
+    std::cout << "Введите угол в градусах: ";
+    std::cin >> x;
+    res = (x / 180) * pi;
+    std::cout << "Результат: " << res;
+}
---
+++ vvp/vvp7/vvp7.2.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <locale.h>
+
+int main() {
+	const double pi = 3.14;
+	double x, res;
+	setlocale(LC_ALL, "Russian");
+	std::cout << "Введите угол в радианах: ";
+	std::cin >> x;
+	res = (180 / pi) * x;
+	std::cout << "Результат: " << res;
+}
